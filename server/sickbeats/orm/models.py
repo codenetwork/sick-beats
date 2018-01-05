0045b46b3f1ba801dbfbbe802cd7af3145cbcbc3
Matthew Miller <mnmiller1@me.com>
2018-01-05 13:03:15 +1000

    Added some models for queues, songs, artists, etc

--- server/sickbeats/orm/models.py
+++ server/sickbeats/orm/models.py
@@ -22,7 +22,55 @@ class Instance(db.Model, IDMixin):
     platform = db.relation('Platform')
 
 
+class Artist(db.Model, IDMixin):
+    __tablename__ = 'artist'
+
+    name = db.Column(db.String, nullable=False)
+
+
+class Album(db.Model, IDMixin):
+    __tablename__ = 'album'
+
+    name = db.Column(db.String, nullable=False)
+
+
 class Platform(db.Model, IDMixin):
     __tablename__ = 'platform'
 
     name = db.Column(db.String, nullable=False)
+
+
+class Song(db.Model):
+    __tablename__ = 'song'
+
+    id = db.Column(db.String, primary_key=True, nullable=False)
+    name = db.Column(db.String, nullable=True)
+    album_id = db.Column(db.Integer, db.ForeignKey('album.id'), nullable=True)
+
+    album = db.relationship('Album')
+
+
+class SongArtistMapping(db.Model, IDMixin):
+    __tablename__ = 'songartistmapping'
+
+    song_id = db.Column(db.String, db.ForeignKey('song.id'), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
+
+    song = db.relationship('Song')
+    artist = db.relationship('Artist')
+
+
+class AlbumArtistMapping(db.Model, IDMixin):
+    __tablename__ = 'albumartistmapping'
+
+    album_id = db.Column(db.Integer, db.ForeignKey('album.id'), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
+
+    album = db.relationship('Album')
+    artist = db.relationship('Artist')
+
+
+class Queue(db.Model, IDMixin):
+    __tablename__ = 'queue'
+
+    song_id = db.Column(db.String, db.ForeignKey('song.id'), nullable=False)
